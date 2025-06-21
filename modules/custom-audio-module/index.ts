76ed4dda5fd44b9c22eacd5fa18e094a274d9cf8
Jozo Mostarac <jozomostarac@gmail.com>
2025-06-21 21:10:47 +0200

    clean-up of new module and make it work

--- modules/custom-audio-module/index.ts
+++ modules/custom-audio-module/index.ts
@@ -1,5 +1,3 @@
-// Reexport the native module. On web, it will be resolved to CustomAudioModule.web.ts
-// and on native platforms to CustomAudioModule.ts
+// Reexport the native module. It will be resolved to CustomAudioModule.ts
 export { default } from './src/CustomAudioModule';
-export { default as CustomAudioModuleView } from './src/CustomAudioModuleView';
 export * from  './src/CustomAudioModule.types';
--- modules/custom-audio-module/src/CustomAudioModule.ts
+++ modules/custom-audio-module/src/CustomAudioModule.ts
@@ -3,9 +3,7 @@ import { NativeModule, requireNativeModule } from 'expo';
 import { CustomAudioModuleEvents } from './CustomAudioModule.types';
 
 declare class CustomAudioModule extends NativeModule<CustomAudioModuleEvents> {
-  PI: number;
   hello(): string;
-  setValueAsync(value: string): Promise<void>;
 }
 
 // This call loads the native module object from the JSI.
